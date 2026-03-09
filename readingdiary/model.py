c35fa6828fcfa66e2d36fcb4e05cf4c87fb4607c
clase 1 <lukasdpikesevg@gmail.com>
2026-03-09 17:02:45 -0500

    Primera Clase, Clase Note

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,3 +1,11 @@
 from datetime import datetime
 
-# TODO: Add code here
+class Note:
+    def __init__(self, text: str, page: int, date: datetime):
+        self.text: str = text
+        self.page: int = page
+        self.date: datetime = date
+
+    def __str__(self) -> str:
+        return f"{self.date} - page {self.page}: {self.date}"
+
